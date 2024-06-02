8edbdf849052b8c468e1f9ac677fc3f8e7b1b0da
Max Yugai <mail@maksimyugai.com>
2024-06-02 13:30:43 +0600

    added 200 response

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketOption;
 
 public class Main {
   public static void main(String[] args) {
@@ -12,11 +13,7 @@ public class Main {
      ServerSocket serverSocket = null;
      Socket clientSocket = null;
 
-     var response = """
-        HTTP/1.1 200 OK
-        
-        
-        """;
+     var response = "HTTP/1.1 200 OK\r\n\r\n";
 
      try {
        serverSocket = new ServerSocket(4221);
@@ -24,7 +21,7 @@ public class Main {
        // ensures that we don't run into 'Address already in use' errors
        serverSocket.setReuseAddress(true);
        clientSocket = serverSocket.accept(); // Wait for connection from client.
-       System.out.println(response);
+       clientSocket.getOutputStream().write(response.getBytes());
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
