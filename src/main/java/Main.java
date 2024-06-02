2e6fa3d36924d3f1363532f2ef0cec4b0dcd7c70
Max Yugai <mail@maksimyugai.com>
2024-06-02 14:22:46 +0600

    added parsing path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,8 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketOption;
 
 public class Main {
   public static void main(String[] args) {
@@ -13,7 +14,8 @@ public class Main {
      ServerSocket serverSocket = null;
      Socket clientSocket = null;
 
-     var response = "HTTP/1.1 200 OK\r\n\r\n";
+     var successResponse = "HTTP/1.1 200 OK\r\n\r\n";
+     var notFoundResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
 
      try {
        serverSocket = new ServerSocket(4221);
@@ -21,7 +23,23 @@ public class Main {
        // ensures that we don't run into 'Address already in use' errors
        serverSocket.setReuseAddress(true);
        clientSocket = serverSocket.accept(); // Wait for connection from client.
-       clientSocket.getOutputStream().write(response.getBytes());
+
+       var requestBuilder = new StringBuilder();
+       var buffReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+       while (buffReader.ready()) {
+         requestBuilder.append(buffReader.readLine()).append("\r\n");
+       }
+
+       var path = requestBuilder.toString().split("\r\n")[0].split(" ")[1].substring(1);
+
+       if (path.isEmpty()) {
+         System.out.println("output: " + successResponse);
+         clientSocket.getOutputStream().write(successResponse.getBytes());
+       } else {
+         System.out.println("output: " + notFoundResponse);
+         clientSocket.getOutputStream().write(notFoundResponse.getBytes());
+       }
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
